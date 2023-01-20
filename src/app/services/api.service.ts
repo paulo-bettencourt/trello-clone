2cca78d88de66fab25a3fd2f9682177c2e429bc1
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-01-19 23:12:04 -0100

    minor updates and bugfixes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,27 +9,19 @@ export class ApiService {
   todo$ = new BehaviorSubject<string[]>( ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'])
   done$ = new BehaviorSubject<string[]>(['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'])
 
-  addNewTask(newTask: string) {
+  addNewTask(newTask: string) {ß
     this.todo$.next([...this.todo$.getValue(), newTask])
   }
 
   deleteTodo(item: string) {
-    // Get current items from the BehaviorSubject.
     const currentItems = this.todo$.getValue();
-    // Use the id of the argument item to remove it from the currentItems.
-    // @ts-ignore
     const itemsWithoutDeleted = currentItems.filter(itemObservable => itemObservable !== item);
-    // Emit the new array.
     this.todo$.next(itemsWithoutDeleted);
   }
 
   deleteDone(item: string) {
-    // Get current items from the BehaviorSubject.
     const currentItems = this.done$.getValue();
-    // Use the id of the argument item to remove it from the currentItems.
-    // @ts-ignore
     const itemsWithoutDeleted = currentItems.filter(itemObservable => itemObservable !== item);
-    // Emit the new array.
     this.done$.next(itemsWithoutDeleted);
   }
 }

fd26310e620034e59a8ce8c76bba774dd38644fa
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-01-19 23:01:13 -0100

    added types

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,40 +1,35 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, merge, Subject} from "rxjs";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  todo$ = new BehaviorSubject( ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'])
-  done$ = new BehaviorSubject(['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'])
+  todo$ = new BehaviorSubject<string[]>( ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'])
+  done$ = new BehaviorSubject<string[]>(['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'])
 
-  constructor() { }
-
-  addTodo(task: any) {
-    console.log("add")
-    this.todo$.next([...this.todo$.getValue(), task])
+  addNewTask(newTask: string) {
+    this.todo$.next([...this.todo$.getValue(), newTask])
   }
 
-  deleteTodo(item: any) {
+  deleteTodo(item: string) {
     // Get current items from the BehaviorSubject.
     const currentItems = this.todo$.getValue();
     // Use the id of the argument item to remove it from the currentItems.
     // @ts-ignore
     const itemsWithoutDeleted = currentItems.filter(itemObservable => itemObservable !== item);
     // Emit the new array.
-    console.log("olé", item)
     this.todo$.next(itemsWithoutDeleted);
   }
 
-  deleteDone(item: any) {
+  deleteDone(item: string) {
     // Get current items from the BehaviorSubject.
     const currentItems = this.done$.getValue();
     // Use the id of the argument item to remove it from the currentItems.
     // @ts-ignore
     const itemsWithoutDeleted = currentItems.filter(itemObservable => itemObservable !== item);
     // Emit the new array.
-    console.log("olé", item)
     this.done$.next(itemsWithoutDeleted);
   }
 }
